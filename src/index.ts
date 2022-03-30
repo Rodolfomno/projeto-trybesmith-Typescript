0e9751714b79ac75a99553fba6989aff40584ae4
Rodolfomno <Rodolfo_mno@hotmail.com>
2022-03-30 01:46:10 -0300

    feat: add connection

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import app from './app';
 
 const PORT = 3000;
 
-const server = app.listen(PORT, () => console.log(
+const server = app.listen(PORT, (): void => console.log(
   `Server is running on PORT: ${PORT}`,
 ));
 
